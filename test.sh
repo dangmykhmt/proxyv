a15ee3a8d0ae2b3f20df0fe222c19201cfe8c055
OPREKIN-PC\Computer <dangmykhmt@gmail.com>
2023-04-05 14:14:03 +0700

    123

--- test.sh
+++ test.sh
@@ -130,7 +130,7 @@ echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}. Network interface nam
 
 #echo "How many proxy do you want to create? Example 500"
 #read COUNT
-COUNT=10
+COUNT=23
 FIRST_PORT=20000
 LAST_PORT=$(($FIRST_PORT + $COUNT))
 
